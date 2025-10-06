5033df3093394fddc6b56d0a6b40fbb5eeaefcf3
forged-by-sim <young.simulationtech@gmail.com>
2025-10-06 12:07:26 -0400

    mod1

---
+++ course1-c-for-everyone-programming-fundamentals/module1-introduction/README_instructions.md
@@ -0,0 +1,20 @@
+Fix Dr. P's mistake assignment
+
+The following program is suppose to write hello world onto the screen but it has syntax errors  - find and correct.
+
+#include<stdio.h>
+
+int main(void) 
+{
+    printf('hello world\n');
+    return 0;
+}
+
+
+Copy the code exactly as is, make your modifications to fix the program, then submit a text file of your code.
+
+
+
+Print a poem 
+
+Use the printf() statement to output a poem of your choice. Submit your program as a text file. 
---
+++ course1-c-for-everyone-programming-fundamentals/module1-introduction/dream_poem.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Take this kiss upon the brow!\\n");
+    printf("And, in parting from you now,\\n");
+    printf("Thus much let me avow --\\n");
+    printf("You are not wrong, who deem\\n");
+    printf("That my days have been a dream;\\n");
+    printf("Yet if hope has flown away\\n");
+    printf("In a night, or in a day,\\n");
+    printf("In a vision, or in none,\\n");
+    printf("Is it therefore the less gone?\\n");
+    printf("All that we see or seem\\n");
+    printf("Is but a dream within a dream.\\n");
+    printf("\\n");
+    printf("I stand amid the roar\\n");
+    printf("Of a surf-tormented shore,\\n");
+    printf("And I hold within my hand\\n");
+    printf("Grains of the golden sand --\\n");
+    printf("How few! yet how they creep\\n");
+    printf("Through my fingers to the deep,\\n");
+    printf("While I weep -- while I weep!\\n");
+    printf("O God! can I not grasp\\n");
+    printf("Them with a tighter clasp?\\n");
+    printf("O God! can I not save\\n");
+    printf("One from the pitiless wave?\\n");
+    printf("Is all that we see or seem\\n");
+    printf("But a dream within a dream?\\n");
+
+    return 0;
+}
